ed551644f8eb4dba96912803e787728bc2201c79
Leander Wernst <leanderwernst@msn.com>
2023-01-04 15:02:05 +0100

    add info text for button states and fade in/fade out value texts

--- Assets/Scripts/Mixer/Button.cs
+++ Assets/Scripts/Mixer/Button.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Button : MonoBehaviour
@@ -14,10 +15,15 @@ public class Button : MonoBehaviour
     private Vector3 endPosition;
     [SerializeField] private float smoothTime = 0.1F;
     [SerializeField] private Vector3 velocity = Vector3.zero;
+    TextMeshProUGUI canvasValueText;
 
+    private void Awake()
+    {
+        canvasValueText = GameObject.FindGameObjectWithTag("ValueText").GetComponent<TextMeshProUGUI>();
+    }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         startPosition = transform.position;
         endPosition = transform.TransformPoint(new Vector3(0, 0, -0.00057567f));
@@ -27,7 +33,7 @@ public class Button : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (isMoving)
         {
@@ -42,12 +48,13 @@ public class Button : MonoBehaviour
 
     private void OnMouseUp()
     {
-            isOn = !isOn;
-            isMoving = true;
-            if (hasLED && isOn) // Turn on LED
-                transform.parent.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
-            else if (hasLED && !isOn)
-                transform.parent.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        isOn = !isOn;
+        isMoving = true;
+        if (hasLED && isOn) // Turn on LED
+            transform.parent.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+        else if (hasLED && !isOn)
+            transform.parent.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        canvasValueText.text = isOn ? "on" : "off";
     }
 
 }

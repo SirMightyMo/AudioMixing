4f1e157d0e8cf51fcc8a2b57bca945ce8c6da7b0
Leander Wernst <leanderwernst@msn.com>
2023-01-06 13:07:09 +0100

    detect if pointer is over UI element

--- Assets/Scripts/Mixer/SelectionManager.cs
+++ Assets/Scripts/Mixer/SelectionManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class SelectionManager : MonoBehaviour
@@ -15,6 +16,7 @@ public class SelectionManager : MonoBehaviour
     private Image valueTextBackground;
     private float vtbMaxAlpha = 235f/255f;
 
+    private int UILayer;
     private enum Fade {Out = 0, In = 1};
 
     private void Awake()
@@ -26,7 +28,7 @@ public class SelectionManager : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
- 
+
     }
 
     // Update is called once per frame
@@ -45,36 +47,39 @@ public class SelectionManager : MonoBehaviour
 
     void HighlightOnHover() 
     {
-        if (!Input.GetMouseButton(0))
-        { 
-            // Turn off emission when not selected
-            if (currentSelection != null && currentSelection != clickedObject)
+        if (!IsPointerOverUIElement())
+        {
+            if (!Input.GetMouseButton(0))
             {
-                var selectionRenderer = currentSelection.GetComponent<Renderer>();
-                selectionRenderer.material.DisableKeyword("_EMISSION");
-                currentSelection = null;
-            }
-
-            // Send ray to mouse position
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            // if ray hits something
-            if (Physics.Raycast(ray, out hit))
-            {
-                Debug.DrawLine(ray.origin, hit.point, Color.green); // DEBUG
-                var selection = hit.transform;
-                // check if selected object has tag contained in array
-                if (TransformWithTagIsMovable(selectableTags, selection))
+                // Turn off emission when not selected
+                if (currentSelection != null && currentSelection != clickedObject)
                 {
-                    // get renderer and if not undefined enable emission
-                    var selectionRenderer = selection.GetComponent<Renderer>();
-                    if (selectionRenderer != null)
+                    var selectionRenderer = currentSelection.GetComponent<Renderer>();
+                    selectionRenderer.material.DisableKeyword("_EMISSION");
+                    currentSelection = null;
+                }
+
+                // Send ray to mouse position
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                // if ray hits something
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Debug.DrawLine(ray.origin, hit.point, Color.green); // DEBUG
+                    var selection = hit.transform;
+                    // check if selected object has tag contained in array
+                    if (TransformWithTagIsMovable(selectableTags, selection))
                     {
-                        selectionRenderer.material.EnableKeyword("_EMISSION");
-                        selectionRenderer.material.SetColor("_EmissionColor", Color.white);
+                        // get renderer and if not undefined enable emission
+                        var selectionRenderer = selection.GetComponent<Renderer>();
+                        if (selectionRenderer != null)
+                        {
+                            selectionRenderer.material.EnableKeyword("_EMISSION");
+                            selectionRenderer.material.SetColor("_EmissionColor", Color.white);
+                        }
+                        // currentSelection is highlighted
+                        currentSelection = selectionRenderer == null ? null : selection;
                     }
-                    // currentSelection is highlighted
-                    currentSelection = selectionRenderer == null ? null : selection;
                 }
             }
         }
@@ -82,52 +87,55 @@ public class SelectionManager : MonoBehaviour
 
     void HighlightOnClick()
     {
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        // if ray hits something
-        if (Physics.Raycast(ray, out hit))
+        if (!IsPointerOverUIElement())
         {
-            Debug.DrawLine(ray.origin, hit.point, Color.red); // DEBUG
-            var selection = hit.transform;
-            if (TransformWithTagIsMovable(selectableTags, selection))
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            // if ray hits something
+            if (Physics.Raycast(ray, out hit))
             {
-                FadeValueText(Fade.In);
-                var clickedBefore = clickedObject;
-                if (clickedBefore != null)
+                Debug.DrawLine(ray.origin, hit.point, Color.red); // DEBUG
+                var selection = hit.transform;
+                if (TransformWithTagIsMovable(selectableTags, selection))
                 {
-                    clickedBefore.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
-                    if (clickedBefore.GetComponent<Fader>() != null)
-                        clickedBefore.GetComponent<Fader>().isClicked = false;
-                    else if (clickedBefore.GetComponent<Button>() != null)
-                        clickedBefore.GetComponent<Button>().isClicked = false;
-                    else if (clickedBefore.GetComponent<Knob>() != null)
-                        clickedBefore.GetComponent<Knob>().isClicked = false;
-                }
+                    FadeValueText(Fade.In);
+                    var clickedBefore = clickedObject;
+                    if (clickedBefore != null)
+                    {
+                        clickedBefore.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+                        if (clickedBefore.GetComponent<Fader>() != null)
+                            clickedBefore.GetComponent<Fader>().isClicked = false;
+                        else if (clickedBefore.GetComponent<Button>() != null)
+                            clickedBefore.GetComponent<Button>().isClicked = false;
+                        else if (clickedBefore.GetComponent<Knob>() != null)
+                            clickedBefore.GetComponent<Knob>().isClicked = false;
+                    }
 
-                clickedObject = selection;
-                var selectionRenderer = clickedObject.GetComponent<Renderer>();
-                selectionRenderer.material.EnableKeyword("_EMISSION");
-                selectionRenderer.material.SetColor("_EmissionColor", Color.white);
-                if (clickedObject.GetComponent<Fader>() != null)
-                    clickedObject.GetComponent<Fader>().isClicked = true;
-                else if (clickedObject.GetComponent<Button>() != null)
-                    clickedObject.GetComponent<Button>().isClicked = true;
-                else if (clickedObject.GetComponent<Knob>() != null)
-                    clickedObject.GetComponent<Knob>().isClicked = true;
-            }
-            else
-            {
-                FadeValueText(Fade.Out);
-                if (clickedObject != null)
-                { 
-                    clickedObject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+                    clickedObject = selection;
+                    var selectionRenderer = clickedObject.GetComponent<Renderer>();
+                    selectionRenderer.material.EnableKeyword("_EMISSION");
+                    selectionRenderer.material.SetColor("_EmissionColor", Color.white);
                     if (clickedObject.GetComponent<Fader>() != null)
-                        clickedObject.GetComponent<Fader>().isClicked = false;
+                        clickedObject.GetComponent<Fader>().isClicked = true;
                     else if (clickedObject.GetComponent<Button>() != null)
-                        clickedObject.GetComponent<Button>().isClicked = false;
+                        clickedObject.GetComponent<Button>().isClicked = true;
                     else if (clickedObject.GetComponent<Knob>() != null)
-                        clickedObject.GetComponent<Knob>().isClicked = false;
-                    clickedObject = null;
+                        clickedObject.GetComponent<Knob>().isClicked = true;
+                }
+                else
+                {
+                    FadeValueText(Fade.Out);
+                    if (clickedObject != null)
+                    { 
+                        clickedObject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+                        if (clickedObject.GetComponent<Fader>() != null)
+                            clickedObject.GetComponent<Fader>().isClicked = false;
+                        else if (clickedObject.GetComponent<Button>() != null)
+                            clickedObject.GetComponent<Button>().isClicked = false;
+                        else if (clickedObject.GetComponent<Knob>() != null)
+                            clickedObject.GetComponent<Knob>().isClicked = false;
+                        clickedObject = null;
+                    }
                 }
             }
         }
@@ -184,6 +192,35 @@ public class SelectionManager : MonoBehaviour
         }
 
     }
-    
+
+
+    //Returns 'true' if we touched or hovering on Unity UI element.
+    public static bool IsPointerOverUIElement()
+    {
+        return IsPointerOverUIElement(GetEventSystemRaycastResults());
+    }
+
+    //Returns 'true' if we touched or hovering on Unity UI element.
+    private static bool IsPointerOverUIElement(List<RaycastResult> eventSystemRaycastResults)
+    {
+        for (int index = 0; index < eventSystemRaycastResults.Count; index++)
+        {
+            RaycastResult curRaycastResult = eventSystemRaycastResults[index];
+            if (curRaycastResult.gameObject.layer == LayerMask.NameToLayer("UI"))
+                return true;
+        }
+        return false;
+    }
+
+    //Gets all event system raycast results of current mouse or touch position.
+    static List<RaycastResult> GetEventSystemRaycastResults()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raysastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raysastResults);
+        return raysastResults;
+    }
+
 
 }

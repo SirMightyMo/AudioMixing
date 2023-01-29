e2d21cf55ae351d16eed552f773a7e941484e04f
Leander Wernst <leanderwernst@msn.com>
2023-01-29 11:38:16 +0100

    fix pip camera script to always adapt to new resolutions

--- Assets/Scripts/PictureInPicture.cs
+++ Assets/Scripts/PictureInPicture.cs
@@ -2,40 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode] // Executes in Edit Mode
 public class PictureInPicture : MonoBehaviour
 {
-    public Camera pictureInPictureCamera;
-    public int pipWidth = 234;
-    public int pipHeight = 484;
-    public int offsetX = 24;
-    public int offsetY = 362;
-    public float scale = 1.0f;
-    private float previousAspectRatio;
+    public float pipWidth = 230f;   // width in px
+    public float pipHeight = 484f;  // height in px
+    private float pipAspectRatio;   // ratio between width/height
+    public float pipRight = 25f;    // offset from right
+    public float pipTop = 235f;     // offset from top
+
+    private Camera pictureInPictureCamera;
+    private int previousScreenWidth;
+    private int previousScreenHeight;
 
     void Start()
     {
-        previousAspectRatio = GetAspectRatio();
+        pictureInPictureCamera = gameObject.GetComponent<Camera>();
+        previousScreenWidth = Screen.width;
+        previousScreenHeight = Screen.height;
     }
-
-
-    //This will keep the pip camera frame with the same dimensions
-    // not depending on changing aspect ratios
     void Update()
     {
-        float currentAspectRatio = GetAspectRatio();
-        // only change pip size when aspect ratio changes
-        if (previousAspectRatio != currentAspectRatio)
+        AdaptPipRect();
+    }
+
+    /**
+     * This function will adapt the shown rectangle of the pip camera 
+     * everytime the resolution changes. It will adapt the physical size 
+     * as well as its position, so it always looks the same.
+     * 
+     * >> NOTE: IF YOU NEED TO CHANGE POSITION OR SIZE CHOOSE 'FULL HD' 
+     *          RESOLUTION IN EDITOR! FULL HD IS THE RESOLUTION THESE 
+     *          CALCULATIONS ARE BASED ON!
+     */
+    private void AdaptPipRect()
+    {
+        if (ScreenResolutionChanged())
         {
-            float aspectRatio = (float)pipWidth / (float)pipHeight;
-            pipWidth = (int)(scale * pipWidth);
-            pipHeight = (int)(pipWidth / aspectRatio);
-            pictureInPictureCamera.pixelRect = new Rect(Screen.width - pipWidth - offsetX, offsetY, pipWidth, pipHeight);
-            previousAspectRatio = currentAspectRatio;
+            // Start (based on FullHD)
+            pipAspectRatio = pipWidth / pipHeight;
+            var screenHeightTo1080Ratio = Screen.height / 1080f;
+
+            // Calculate new resolution and position
+            var pipHeightNew = pipHeight * screenHeightTo1080Ratio;
+            var pipWidthNew = pipAspectRatio * pipHeightNew;
+
+            var pipNewX = Screen.width - pipWidthNew - pipRight * screenHeightTo1080Ratio;
+            var pipNewY = Screen.height - pipHeightNew - pipTop * screenHeightTo1080Ratio;
+
+            pictureInPictureCamera.pixelRect = new Rect(pipNewX, pipNewY, pipWidthNew, pipHeightNew);
         }
     }
 
-    private float GetAspectRatio()
+    private bool ScreenResolutionChanged()
     {
-        return Screen.width / (float)Screen.height;
+        if (previousScreenWidth != Screen.width || previousScreenHeight != Screen.height)
+        {
+            previousScreenWidth = Screen.width;
+            previousScreenHeight = Screen.height;
+            return true;
+        }
+        return false;
     }
 }

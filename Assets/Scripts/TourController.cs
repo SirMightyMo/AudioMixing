7cf6c65c9e367be7f05c0e7a94ac1d9a7c7fcc65
Leander Wernst <leanderwernst@msn.com>
2023-01-31 16:03:36 +0100

    adjusted cameras for training mode

--- Assets/Scripts/TourController.cs
+++ Assets/Scripts/TourController.cs
@@ -5,12 +5,9 @@ using Cinemachine;
 public class TourController : MonoBehaviour
 {
     public CinemachineBrain cine;
-    public CinemachineVirtualCamera vCamOverview;
     public CinemachineVirtualCamera vCam0;
-    public CinemachineVirtualCamera vCam3;
-    public CinemachineVirtualCamera vCam4;
-    public CinemachineVirtualCamera vCam21;
-    public CinemachineVirtualCamera vCam27;
+    public CinemachineVirtualCamera vCam1;
+    public CinemachineVirtualCamera vCam2;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,29 +24,17 @@ public class TourController : MonoBehaviour
     {
         switch (cine.ActiveVirtualCamera.Name)
         {
-            case "CMvcamOverview":
-                vCamOverview.Priority = 1;
-                vCam0.Priority = 2;
-                break;
             case "CMvcam0":
                 vCam0.Priority = 1;
-                vCam3.Priority = 2;
+                vCam1.Priority = 2;
                 break;
-            case "CMvcam3":
-                vCam3.Priority = 1;
-                vCam4.Priority = 2;
+            case "CMvcam1":
+                vCam1.Priority = 1;
+                vCam2.Priority = 2;
                 break;
-            case "CMvcam4":
-                vCam4.Priority = 1;
-                vCam21.Priority = 2;
-                break;
-            case "CMvcam21":
-                vCam21.Priority = 1;
-                vCam27.Priority = 2;
-                break;
-            case "CMvcam27":
-                vCam27.Priority = 1;
-                vCamOverview.Priority = 2;
+            case "CMvcam2":
+                vCam2.Priority = 1;
+                vCam0.Priority = 2;
                 break;
             default:
                 // do default
